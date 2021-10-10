70e4f166c1cd94b04fc1cffe49253677815b7a32
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-10-10 23:45:02 +0700

    config tailwin again

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        josefins: ['Josefin Sans']
+        poppins: ['Poppins']
       },
       colors: {
         transparent: 'transparent',
