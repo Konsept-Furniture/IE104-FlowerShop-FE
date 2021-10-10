b303ae3844b9b6d1958ed6d537086ac6402fe80b
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-10-10 20:54:45 +0700

    change config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,43 +2,43 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fontFamily:{
-      josefins: ['Josefin Sans']
+    extend: {
+      fontFamily: {
+        josefins: ['Josefin Sans']
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        white: {
+          DEFAULT: '#FFFFFF',
+        },
+        blue: {
+          light: '#85d7ff',
+          DEFAULT: '#1fb6ff',
+          dark: '#009eeb',
+        },
+        pink: {
+          light: '#ff7ce5',
+          DEFAULT: '#ff49db',
+          dark: '#ff16d1',
+        },
+        gray: {
+          darkest: '#1f2d3d',
+          dark: '#3c4858',
+          DEFAULT: '#c0ccda',
+          light: '#e0e6ed',
+          lightest: '#f9fafc',
+          footer: '#f4f2f2',
+        },
+        konsept: {
+          DEFAULT: '#1d1d1d',
+          gray: '#8b8b8b'
+        },
+        footer_bg: {
+          DEFAULT: '#f4f2f2',
+        }
+      },
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      white:{
-        DEFAULT: '#FFFFFF',
-      },
-      blue: {
-        light: '#85d7ff',
-        DEFAULT: '#1fb6ff',
-        dark: '#009eeb',
-      },
-      pink: {
-        light: '#ff7ce5',
-        DEFAULT: '#ff49db',
-        dark: '#ff16d1',
-      },
-      gray: {
-        darkest: '#1f2d3d',
-        dark: '#3c4858',
-        DEFAULT: '#c0ccda',
-        light: '#e0e6ed',
-        lightest: '#f9fafc',
-        footer:'#f4f2f2',
-      },
-      konsept: {
-        DEFAULT:'#1d1d1d',
-        gray: '#8b8b8b'
-      },
-      footer_bg: {
-        DEFAULT: '#f4f2f2',
-
-      }
-    },
-    extend: {},
   },
   variants: {
     extend: {},
