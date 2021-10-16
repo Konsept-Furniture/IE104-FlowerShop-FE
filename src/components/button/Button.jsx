a68d2771949d2f1ea3942546fd5677761f301090
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-10-16 18:39:11 +0700

    add button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Button.scss';
+
+const Button = ({ className, onClick, children }) => {
+    return (
+        <button
+            className={`btn ${className}`}
+            onClick={onClick ? () => onClick() : null}
+        >
+            {children}
+        </button>
+    );
+}
+
+export const OutlinedButton = props => {
+    return (
+        <Button
+            className={`btn-outlined ${props.className}`}
+            onClick={props.onClick ? () => props.onClick() : null}
+        >
+            {props.children}
+        </Button>
+    );
+}
+
+Button.propTypes = {
+    onClick: PropTypes.func
+}
+
+export default Button;
