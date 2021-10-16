2c3708b7bc1cc16cc5859bff9bbfe4966bebd7b0
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-10-16 18:39:56 +0700

    change header nav items to list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,40 @@ import IconCart from '@/assets/icons/IconCart';
 import IconUser from '@/assets/icons/IconUser';
 import IconBurger from '@/assets/icons/IconBurger';
 import './Header.scss';
+import { path } from '@/constants/path';
 
 Header.propTypes = {};
 
 function Header(props) {
+   const navItems = [
+      {
+         label: "Home",
+         route: path.home
+      },
+      {
+         label: "Pages",
+         route: path.shop
+      },
+      {
+         label: "Shop",
+         route: path.shop
+      },
+      {
+         label: "Store",
+         route: path.shop
+      },
+      {
+         label: "Portfolio",
+         route: path.shop
+      },
+      {
+         label: "Blog",
+         route: path.shop
+      },
+   ]
    return (
-      <header className="header py-4">
-         <div class="header--innner konsept-container flex justify-between items-center">
+      <header className="header py-4 font-poppins">
+         <div className="header--innner konsept-container flex justify-between items-center">
             <div className="mr-4 lg:mr-16 h-14">
                <img
                   className="h-full"
@@ -25,36 +52,13 @@ function Header(props) {
 
             <nav className="header__nav mr-auto hidden lg:block">
                <ul className="flex items-center">
-                  <li>
-                     <NavLink to="/" className="konsept-link">
-                        Home
-                     </NavLink>
-                  </li>
-                  <li>
-                     <NavLink to="/pages" className="konsept-link">
-                        Pages
-                     </NavLink>
-                  </li>
-                  <li>
-                     <NavLink to="/shop" className="konsept-link">
-                        Shop
-                     </NavLink>
-                  </li>
-                  <li>
-                     <NavLink to="/store" className="konsept-link">
-                        Store
-                     </NavLink>
-                  </li>
-                  <li>
-                     <NavLink to="/portfolio" className="konsept-link">
-                        Portfolio
-                     </NavLink>
-                  </li>
-                  <li>
-                     <NavLink to="/blog" className="konsept-link">
-                        Blog
-                     </NavLink>
-                  </li>
+                  {navItems.map(item => (
+                     <li key={item.label}>
+                        <NavLink to={item.route} className="konsept-link">
+                           {item.label}
+                        </NavLink>
+                     </li>
+                  ))}
                </ul>
             </nav>
 
