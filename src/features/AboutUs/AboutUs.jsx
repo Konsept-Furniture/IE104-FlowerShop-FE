df23b28cd4ec4d8f1e6d19c429e359484e3fb08e
Nhien Nguyen <annhien.se@gmail.com>
2021-11-06 17:34:43 +0700

    feat: add partnership bar

--- src/features/AboutUs/AboutUs.jsx
+++ src/features/AboutUs/AboutUs.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import AboutUsHeader from '../../components/AboutUs/AboutUsHeader'
+import AboutUsLogo from '@/components/AboutUs/AboutUsLogo'
 
 AboutUs.propTypes = {
+
 }
 
 function AboutUs (props) {
-  const background = 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/About-us_title.jpg'
   return (
-    <div className="w-full bg-cover bg-center h-96" style ={{ backgroundImage: 'url(' + background + ')' } }>
-      <div className="flex items-center justify-center h-full w-full bg-opacity-50">
-            <div className="text-center">
-                <h1 className="text-white text-2xl font-josefins font-bold tracking-widest uppercase md:text-4xl">ABOUT US</h1>
-            </div>
+        <div>
+            <AboutUsHeader/>
+            <AboutUsLogo/>
         </div>
-    </div>
   )
 }
 
