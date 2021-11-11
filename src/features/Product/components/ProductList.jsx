f80e8871676e2722176a659d4a1920c1f0cd6ef5
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-11 18:05:10 +0700

    fix: responsive product list page for 3 column in laptop

--- src/features/Product/components/ProductList.jsx
+++ src/features/Product/components/ProductList.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import ProductItem from './ProductItem'
+import { Box } from '@mui/material'
 import Grid from '@mui/material/Grid'
+import PropTypes from 'prop-types'
+import React from 'react'
+import ProductItem from './ProductItem'
 
 ProductList.propTypes = {
    data: PropTypes.array.isRequired
@@ -9,13 +10,18 @@ ProductList.propTypes = {
 
 function ProductList({ data }) {
    return (
-      <Grid container>
-         {data.map(product => (
-            <Grid item key={product._id} xs={12} sm={6} md={4} lg={3}>
-               <ProductItem product={product}/>
-            </Grid>
-         ))}
-      </Grid>
+      <div>
+         <Box pl="10px" mb={3} mt={2} className="text--italic color--gray">
+            <h5>Showing 1–9 of 90 results</h5>
+         </Box>
+         <Grid container>
+            {data.map(product => (
+               <Grid item key={product._id} xs={12} sm={6} md={4} lg={4}>
+                  <ProductItem product={product}/>
+               </Grid>
+            ))}
+         </Grid>
+      </div>
    )
 }
 
--- src/features/Product/components/ProductSkeletonList.jsx
+++ src/features/Product/components/ProductSkeletonList.jsx
@@ -14,32 +14,37 @@ ProductSkeletonList.defaultProps = {
 
 function ProductSkeletonList({ length }) {
    return (
-      <Grid container>
-         {Array.from(new Array(length)).map((_, index) => (
-            <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
-               <Box px={2.5} mb={6}>
-                  <Box>
-                     <Skeleton variant="rectangular" width="100%" sx={{
-                        minHeight: {
-                           lg: '250px',
-                           sm: '250px'
-
-                        }
-                     }}/>
-                  </Box>
-                  <Box className="product__info--skeleton" mt={0.5}>
-                     <Box width="40%">
-                        <Skeleton height={50}/>
-                        {/* <Skeleton height={25} mt={-1}/> */}
+      <>
+         <Box pl="10px" mb={2}>
+            <Skeleton height={50} width="30%"/>
+         </Box>
+         <Grid container>
+            {Array.from(new Array(length)).map((_, index) => (
+               <Grid item key={index} xs={12} sm={6} md={4} lg={4}>
+                  <Box px={1.2} mb={6}>
+                     <Box>
+                        <Skeleton variant="rectangular" width="100%" sx={{
+                           minHeight: {
+                              lg: '350px',
+                              md: '350px',
+                              sm: '450px',
+                              xs: '450px'
+                           }
+                        }}/>
                      </Box>
-                     <Box width="25%">
-                        <Skeleton height={35}/>
+                     <Box className="product__info--skeleton" mt={0.5}>
+                        <Box width="40%">
+                           <Skeleton height={50}/>
+                        </Box>
+                        <Box width="25%">
+                           <Skeleton height={35}/>
+                        </Box>
                      </Box>
                   </Box>
-               </Box>
-            </Grid>
-         ))}
-      </Grid>
+               </Grid>
+            ))}
+         </Grid>
+      </>
    )
 }
 
