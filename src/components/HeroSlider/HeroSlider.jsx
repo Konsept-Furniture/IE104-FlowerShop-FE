d5f0986fe46486645dee4366f44d44cab26b171a
annhienktuit <annhien.se@gmail.com>
2021-11-11 08:27:53 +0700

    feat: add slider auto swipe and desescrption

--- src/components/HeroSlider/HeroSlider.jsx
+++ src/components/HeroSlider/HeroSlider.jsx
@@ -57,6 +57,7 @@ function HeroSlider() {
             modules={[Autoplay, EffectFade]}
             spaceBetween={0}
             slidesPerView={1}
+            autoplay={{ delay: 5000 }}
             // navigation
             loop
             effect={'fade'}
