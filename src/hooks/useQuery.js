2121016e30e1d0b329e69709bbdb4471b0e4236c
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-11 16:07:01 +0700

    add useQuery

--- src/hooks/useQuery.js
+++ src/hooks/useQuery.js
@@ -2,11 +2,11 @@ import { useMemo } from 'react'
 import { useLocation } from 'react-router-dom'
 import qs from 'query-string'
 
-export default function useQuery () {
-  const location = useLocation()
-  const queryString = useMemo(
-    () => qs.parse(location.search),
-    [location.search]
-  )
-  return queryString
+export default function useQuery() {
+   const location = useLocation()
+   const queryString = useMemo(
+      () => qs.parse(location.search),
+      [location.search]
+   )
+   return queryString
 }
