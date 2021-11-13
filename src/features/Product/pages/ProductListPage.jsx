ac618fbb166e896c2ec010fee000051a2503ecf1
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-13 09:50:53 +0700

    load product list + refactor code

--- src/features/Product/pages/ProductListPage.jsx
+++ src/features/Product/pages/ProductListPage.jsx
@@ -3,7 +3,9 @@ import React, { useEffect, useState } from 'react'
 import FilterPanel from '../components/Filter/FilterPanel'
 import ProductList from '../components/ProductList'
 import ProductSkeletonList from '../components/ProductSkeletonList'
+import productApi from '../productApi'
 import './ProductListPage.scss'
+import { Box, Skeleton } from '@mui/material'
 
 function ProductListPage() {
    const [loading, setLoading] = useState(true)
@@ -36,76 +38,26 @@ function ProductListPage() {
       maxPrice: queryParams.maxPrice || 100
    }
 
-   const data = [
-      {
-         _id: 0,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 1,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 2,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 3,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 4,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 5,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 6,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 7,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
-      },
-      {
-         _id: 8,
-         name: 'Dresser',
-         imageUrl: 'https://konsept.qodeinteractive.com/wp-content/uploads/2020/04/shoplist7-600x811.jpg',
-         category: 'Bedroom',
-         price: '96.00'
+   const [data, setData] = useState([])
+
+   const getProducts = async() => {
+      setLoading(true)
+      try {
+         const payload = {
+            page: 1,
+            size: 10
+         }
+         const res = await productApi.getProducts(payload)
+         console.log(res)
+         setData(res.products)
+      } catch (error) {
+         console.log(error)
       }
-   ]
+      setLoading(false)
+   }
 
    useEffect(() => {
-      setTimeout(() => {
-         setLoading(false)
-      }, 3000)
+      getProducts()
    }, [queryParams])
 
    return (
@@ -114,6 +66,12 @@ function ProductListPage() {
             <FilterPanel categories={categories} filters={filters}/>
          </div>
          <div className="products__list">
+            <Box pl="10px" mb={3} mt={2} className="text--italic color--gray">
+               {loading
+                  ? <Skeleton width="180px" height="25px"/>
+                  : <h5>Showing 1–9 of 90 results</h5>
+               }
+            </Box>
             {loading
                ? <ProductSkeletonList />
                : <ProductList data={data}/>
