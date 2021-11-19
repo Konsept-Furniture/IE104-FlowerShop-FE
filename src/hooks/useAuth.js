7ee4017e11d2fe6009bb045f0bf67075b402e212
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-11-20 01:10:55 +0700

    feat: add useAuth hook to check logged user

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react'
+import authApi from '@/features/Auth/authApi'
+
+export function useAuth() {
+   const [hasLoggedIn, setHasLoggedIn] = useState(true)
+   const [isLoading, setIsLoading] = useState(true)
+
+   useEffect(() => {
+      (async() => {
+         try {
+            await authApi.checkAccessToken()
+         } catch (error) {
+            setHasLoggedIn(false)
+         }
+         setIsLoading(false)
+      })()
+   }, [])
+
+   return {
+      hasLoggedIn,
+      isLoading
+   }
+}
