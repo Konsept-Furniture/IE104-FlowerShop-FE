83ea49696c36d2d6b164c4b743b983465daadc01
annhienktuit <19520207@gm.uit.edu.vn>
2021-11-26 23:29:47 +0700

    feat: add contact form

--- src/features/AboutUs/AboutUs.jsx
+++ src/features/AboutUs/AboutUs.jsx
@@ -4,20 +4,22 @@ import AboutUsHeader from '../../components/AboutUs/AboutUsHeader'
 import AboutUsLogo from '@/components/AboutUs/AboutUsLogo'
 import AboutUsTeam from '@/components/AboutUs/AboutUsTeam'
 import AboutUsComment from '@/components/AboutUs/AboutUsComment'
+import ContactForm from '../ContactForm/ContactForm'
 
 AboutUs.propTypes = {
 
 }
 
-function AboutUs (props) {
-  return (
-        <div>
-            <AboutUsHeader/>
-            <AboutUsComment/>
-            <AboutUsTeam/>
-            <AboutUsLogo/>
-        </div>
-  )
+function AboutUs(props) {
+   return (
+      <div>
+         <AboutUsHeader/>
+         <AboutUsComment/>
+         <AboutUsTeam/>
+         <AboutUsLogo/>
+         <ContactForm/>
+      </div>
+   )
 }
 
 export default AboutUs
