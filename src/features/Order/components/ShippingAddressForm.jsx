21ceb5cc10ecef3fbd144aa4315bad685907c5d5
unknown <annhien.se@gmail.com>
2021-12-05 12:57:35 +0700

    fix:spelling fix

--- src/features/Order/components/ShippingAddressForm.jsx
+++ src/features/Order/components/ShippingAddressForm.jsx
@@ -137,7 +137,7 @@ function ShippingAddressForm({ defaultValues, onSubmit }) {
    }
    return (
       <form onSubmit={handleSubmit(handleSubmitOrder)}>
-         <Typography variant="h4">Shipping Information</Typography>
+         <Typography variant="h4">Delivery Information</Typography>
 
          <TextInputField
             label="Recipient's name"
@@ -174,7 +174,7 @@ function ShippingAddressForm({ defaultValues, onSubmit }) {
          <SelectPaymentMethod name="payment" control={control} />
 
          <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
-            <PrimaryButton type="submit">Place Order</PrimaryButton>
+            <PrimaryButton type="submit">Confirm Order</PrimaryButton>
          </Box>
       </form>
    )
