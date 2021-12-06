6c6c914962406df1f183e094b9a140c1a019df4d
unknown <annhien.se@gmail.com>
2021-12-06 08:52:40 +0700

    fix: change images to local assets

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -2,5 +2,11 @@ export const IMAGES = {
    AuthCover: require('./auth-cover.jpg').default,
    CartEmpty: require('./empty.png').default,
    MyAccount: require('./my-account.jpg').default,
-   AboutUs: require('./about-us.jpg').default
+   AboutUs: require('./about-us.jpg').default,
+   Nhien: require('./nhien.jpg').default,
+   Khoa: require('./khoa.jpg').default,
+   Binh: require('./binh.jpg').default,
+   Sang: require('./sang.jpg').default,
+   Hien: require('./hien.jpg').default,
+   Tuyen: require('./tuyen.jpg').default
 }
