5907f2f2ae5351452c30dd3c54de8e6511b2ea7e
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-12-07 01:57:13 +0700

    feat: load default delivery info when order

--- src/features/Order/components/ShippingAddressForm.jsx
+++ src/features/Order/components/ShippingAddressForm.jsx
@@ -1,19 +1,26 @@
+/* eslint-disable multiline-ternary */
+/* eslint-disable react/jsx-no-undef */
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import TextInputField from '@/components/form-controls/TextInputField'
 import * as yup from 'yup'
-import { useForm } from 'react-hook-form'
+import { useForm, useWatch } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import PrimaryButton from '@/components/button/Button'
 import AddressField from '@/components/form-controls/AddressField'
-import { Stack, Typography } from '@mui/material'
+import { CircularProgress, Divider, Stack, Typography } from '@mui/material'
 import provinceApi from '@/api/provinceApi'
 import { common } from '@/utils/common'
 import CheckboxField from '@/components/form-controls/CheckboxField'
 import SelectPaymentMethod from './PaymentMethod'
 import { PaymentMethod } from '@/constants/enum'
 import { Box } from '@mui/system'
-
+import { useSelector } from 'react-redux'
+import { usePayPalScriptReducer } from '@paypal/react-paypal-js'
+import ReactPayPal from '@/components/ReactPayPal/ReactPayPal'
+import { useSnackbar } from 'notistack'
+import messages from '@/constants/messages'
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd'
 ShippingAddressForm.propTypes = {
    onSubmit: PropTypes.func.isRequired,
    defaultValues: PropTypes.object
@@ -32,9 +39,21 @@ const schema = yup.object().shape({
       ),
    email: yup.string().email().max(255).required().label('Email address'),
    shippingAddress: yup.object().shape({
-      province: yup.number().required().label('Province'),
-      district: yup.number().required().label('District'),
-      ward: yup.number().required().label('Ward'),
+      province: yup
+         .number()
+         .required()
+         .label('Province')
+         .typeError('Province is a required field'),
+      district: yup
+         .number()
+         .required()
+         .label('District')
+         .typeError('District is a required field'),
+      ward: yup
+         .number()
+         .required()
+         .label('Ward')
+         .typeError('Ward is a required field'),
       street: yup.string().required().label('Street')
    }),
    save: yup.boolean(),
@@ -43,6 +62,9 @@ const schema = yup.object().shape({
 })
 
 function ShippingAddressForm({ defaultValues, onSubmit }) {
+   const { deliveryInfo } = useSelector(state => state.auth.profile)
+   const { isPaid } = useSelector(state => state.order)
+   const { enqueueSnackbar } = useSnackbar()
    const [provinceList, setProvinceList] = useState([])
    const [districtList, setDistrictList] = useState([])
    const [wardList, setWardList] = useState([])
@@ -64,7 +86,13 @@ function ShippingAddressForm({ defaultValues, onSubmit }) {
       },
       resolver: yupResolver(schema)
    })
-   const { control, handleSubmit } = form
+   const { control, handleSubmit, reset } = form
+
+   const watchPayment = useWatch({
+      control,
+      name: 'payment'
+   })
+   const [{ isPending }] = usePayPalScriptReducer()
 
    useEffect(() => {
       ;(async () => {
@@ -112,9 +140,53 @@ function ShippingAddressForm({ defaultValues, onSubmit }) {
       }
    }
 
+   const handleLoadDefaultDeliveryInfo = async () => {
+      ;(async () => {
+         const province = deliveryInfo.address.province
+         if (province) {
+            try {
+               const res = await provinceApi.getDistricts(province)
+               setDistrictList(
+                  res.data.districts.map(item => ({
+                     label: item.name,
+                     value: item.code
+                  }))
+               )
+            } catch (error) {
+               console.log('error to get province list', error)
+            }
+         }
+      })()
+      ;(async () => {
+         const district = deliveryInfo.address.district
+         if (district) {
+            try {
+               const res = await provinceApi.getWards(district)
+               setWardList(
+                  res.data.wards.map(item => ({
+                     label: item.name,
+                     value: item.code
+                  }))
+               )
+            } catch (error) {
+               console.log('error to get province list', error)
+            }
+         }
+      })()
+      reset({
+         ...deliveryInfo,
+         shippingAddress: deliveryInfo.address,
+         payment: PaymentMethod.COD
+      })
+   }
+
    const handleSubmitOrder = async values => {
       console.log('submit order', values)
-      if (onSubmit) {
+      if (watchPayment === 'PayPal' && !isPaid) {
+         enqueueSnackbar(messages.PAYMENT_REQUIRED, {
+            variant: 'error'
+         })
+      } else if (onSubmit) {
          const province = provinceList.find(
             item =>
                item.value === Number.parseInt(values.shippingAddress.province)
@@ -134,6 +206,7 @@ function ShippingAddressForm({ defaultValues, onSubmit }) {
          }
          await onSubmit({
             ...values,
+            isPaid,
             address: shippingAddress,
             address_code: {
                street: values.shippingAddress.street,
@@ -146,7 +219,23 @@ function ShippingAddressForm({ defaultValues, onSubmit }) {
    }
    return (
       <form onSubmit={handleSubmit(handleSubmitOrder)}>
-         <Typography variant="h4">Delivery Information</Typography>
+         <Stack
+            direction="row"
+            justifyContent="space-between"
+            alignItems="flex-end"
+         >
+            <Typography variant="h4">Delivery Information</Typography>
+
+            {deliveryInfo.address.street && (
+               <Typography
+                  variant="subtitle1"
+                  component="a"
+                  onClick={handleLoadDefaultDeliveryInfo}
+               >
+                  Use your default delivery information? <AssignmentIndIcon />
+               </Typography>
+            )}
+         </Stack>
 
          <TextInputField
             label="Recipient's name"
@@ -182,6 +271,26 @@ function ShippingAddressForm({ defaultValues, onSubmit }) {
          <Typography variant="h4">Payment Method</Typography>
 
          <SelectPaymentMethod name="payment" control={control} />
+
+         {watchPayment === 'PayPal' && (
+            <div
+               style={{ maxWidth: '750px', minHeight: '200px', marginTop: 20 }}
+            >
+               <Divider sx={{ mb: 3 }} />
+               {isPending ? (
+                  <Box
+                     sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                     }}
+                  >
+                     <CircularProgress color="black" />
+                  </Box>
+               ) : null}
+               <ReactPayPal currency={'USD'} showSpinner={false} />
+            </div>
+         )}
 
          <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 3 }}>
             <PrimaryButton type="submit">Confirm Order</PrimaryButton>
