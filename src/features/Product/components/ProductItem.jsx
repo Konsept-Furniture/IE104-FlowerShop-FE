ca1eeeb343714b6b57d132b99ac8073e195e9696
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-12-08 00:24:48 +0700

    feat: load all category in product item card

--- src/features/Product/components/ProductItem.jsx
+++ src/features/Product/components/ProductItem.jsx
@@ -1,5 +1,6 @@
 import IconEye from '@/assets/icons/IconEye'
 import IconHeart from '@/assets/icons/IconHeart'
+import { path } from '@/constants/path'
 import { CircularProgress } from '@mui/material'
 import PropTypes from 'prop-types'
 import React, { useState } from 'react'
@@ -47,20 +48,14 @@ function ProductItem({ product, onAddCart }) {
                <div className="overlay__add-cart">
                   {product.quantity > 0 ? (
                      !loading ? (
-                        <a
-                           className="konsept-link text--italic"
-                           onClick={handleAddToCart}
-                        >
+                        <a className="konsept-link text--italic" onClick={handleAddToCart}>
                            Add To Cart
                         </a>
                      ) : (
                         <CircularProgress color="black" size={24} />
                      )
                   ) : (
-                     <a
-                        className="konsept-link text--italic"
-                        onClick={handleReadMore}
-                     >
+                     <a className="konsept-link text--italic" onClick={handleReadMore}>
                         Read more
                      </a>
                   )}
@@ -72,12 +67,27 @@ function ProductItem({ product, onAddCart }) {
             <div className="product__info">
                <h5 className="product__info-title">{product.title}</h5>
                <div className="product__info-category">
-                  <a>{product.categories[0]}</a>
+                  <a>
+                     {product.categories.map((cate, idx) => (
+                        <span key={cate}>
+                           {idx !== 0 ? ', ' : ''}
+                           <a
+                              onClick={() => {
+                                 history.push({
+                                    pathname: path.products,
+                                    search: `?category=${cate}`
+                                 })
+                              }}
+                              style={{ textTransform: 'capitalize' }}
+                           >
+                              {cate}
+                           </a>
+                        </span>
+                     ))}
+                  </a>
                </div>
             </div>
-            <div className="product__info-price">
-               ${product.price.toFixed(2)}
-            </div>
+            <div className="product__info-price">${product.price.toFixed(2)}</div>
          </div>
       </div>
    )
