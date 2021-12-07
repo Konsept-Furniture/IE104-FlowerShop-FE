803c7a3f1cc82f687d75c9f2b0e9b2717421685a
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2021-12-07 23:59:23 +0700

    feat: handle empty order

--- src/features/User/components/tabs/OrdersTrackingTab.jsx
+++ src/features/User/components/tabs/OrdersTrackingTab.jsx
@@ -2,40 +2,58 @@
 import orderApi from '@/api/orderApi'
 import { CircularProgress, Typography } from '@mui/material'
 import { Box } from '@mui/system'
+import { useSnackbar } from 'notistack'
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import AccordionOrderTracking from '../AccordionOrderTracking'
 
-OrdersTrackingTab.propTypes = {}
-
-function OrdersTrackingTab(props) {
+function OrdersTrackingTab() {
+   const { enqueueSnackbar } = useSnackbar()
    const [orders, setOrders] = useState([])
+   const [isEmpty, setIsEmpty] = useState(false)
+
    useEffect(() => {
       ;(async () => {
          try {
             const res = await orderApi.getAll()
-            setOrders(res.data)
-         } catch (error) {}
+            if (res.data.length > 0) {
+               setOrders(res.data)
+            } else {
+               setIsEmpty(true)
+            }
+         } catch (error) {
+            enqueueSnackbar(error.message, {
+               variant: 'error'
+            })
+         }
       })()
    }, [])
+
    return (
       <div>
          <Typography variant="h4" sx={{ mb: 2 }}>
             Orders tracking
          </Typography>
+
          {orders.length > 0 ? (
-            orders.map((order, i) => (
-               <AccordionOrderTracking order={order} key={order._id} />
-            ))
+            orders.map((order, i) => <AccordionOrderTracking order={order} key={order._id} />)
          ) : (
             <Box
                sx={{
-                  pt: 10,
+                  pt: 8,
                   display: 'flex',
                   alignItems: 'center',
                   justifyContent: 'center'
                }}
             >
-               <CircularProgress color="black" />
+               {!isEmpty ? (
+                  <CircularProgress color="black" />
+               ) : (
+                  <Typography variant="h5">
+                     No order found. <Link style={{ textDecoration: 'underline' }}>Shopping</Link>{' '}
+                     now!
+                  </Typography>
+               )}
             </Box>
          )}
       </div>
